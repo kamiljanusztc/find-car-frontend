6bcc0c2abc756cbe1c6a5ec826f132b4cfdbcf6c
Kamil Janusz <kamiljanusztc@gmail.com>
2022-10-19 10:28:31 +0200

    project init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './cards/header/header.component';
+import { FooterComponent } from './cards/footer/footer.component';
+import { ProjectComponent } from './containers/project/project.component';
+import { ButtonPrimaryComponent } from './components/button-primary/button-primary.component';
+import { ButtonSecondaryComponent } from './components/button-secondary/button-secondary.component';
+import { ProductComponent } from './cards/product/product.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProjectComponent,
+    ButtonPrimaryComponent,
+    ButtonSecondaryComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
